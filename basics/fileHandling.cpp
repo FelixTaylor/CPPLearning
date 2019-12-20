21588c92028070fca27b22f217f66272aa598d1b
PfoRsten <marvin-hauke@hotmail.de>
2019-12-20 17:06:23 +0100

    commented

--- basics/fileHandling.cpp
+++ basics/fileHandling.cpp
@@ -4,12 +4,15 @@
 using namespace std;
 
 int main() {
-
+    //Ofstream is a Handler for writting and reading to textfiles
+    //Ofstream.open öffnet eine text Datei mit dem entsprechenden namen
+    //ofstream schreibt in die Textdatei
+    //ofs.close() is important to get rescoures back!
     ofstream ofs;
     ofs.open("test.txt");
 
     if (ofs.is_open()) {
-        ofs << "Hallo Person";
+	ofs << "Hallo Person";
         ofs.close();
     }
 
