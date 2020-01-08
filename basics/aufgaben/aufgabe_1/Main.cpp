ff2c780645cbfebea7ecb0795918b75cb1247531
FelixTaylor <fetalix@riseup.net>
2020-01-08 20:09:22 +0100

    Session 2

--- basics/aufgaben/aufgabe_1/Main.cpp
+++ basics/aufgaben/aufgabe_1/Main.cpp
@@ -1,8 +1,15 @@
 #include<iostream>
-
-usign namespace std;
-
+//#include<stdio.h>
 int main() {
+    char name[25];
+
+    //std::cout << "Wie heißt du?" << std::endl;
+    //std::cin >> name;
+    //std::cout << "Hallo " << name << "!" << std::endl;
+
+    printf("Hallo %s\n",name);
+    printf("Wie heißt du?\n");
+    scanf("%24s", name);
 
     return 0;
 }
--- basics/aufgaben/aufgabe_2/Main.cpp
+++ basics/aufgaben/aufgabe_2/Main.cpp
@@ -1,8 +1,31 @@
 #include<iostream>
-
-usign namespace std;
+#include <stdio.h>
 
 int main() {
+    float zahl {};
+    char name [25] {};
+    char buchstabe {};
+    
+    // std::cout<< "gib einen Namen ein:" << std::endl;
+    // std::cin >> name;
+    // std::cout << "gib eine Zahl ein:" << std::endl;
+    // std::cin >> zahl;
+    // std::cout << "gib einen Buchstaben ein:" << std::endl;
+    // std::cin >> buchstabe;
+    // std::cout << "deine eingegebenen Daten sind:" << std::endl;
+    // std::cout << name << ", " << zahl << ", " << buchstabe << std::endl;
+    // std::cout << zahl << std::endl;
 
+    printf("Gib einen Namen ein:");
+    scanf("%24s",name);
+    printf("Gib eine Zahl ein:");    
+    scanf("%f", &zahl);
+    printf("Gib einen Buchstaben ein:");
+    scanf("%s", &buchstabe);
+
+    // Ausgabe
+    printf("\nDein angegebener Name: %s\n", name);
+    printf("Dein angegebene Zahl: %f\n", zahl);
+    printf("Dein angegebener Buchstabe: %s\n", &buchstabe);
     return 0;
 }
